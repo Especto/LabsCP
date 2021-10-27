ee2d72cee554c3ca03de01fc441e6f8fc4bdfdde
Fonsberg <46784115+Especto@users.noreply.github.com>
2021-10-27 13:05:05 +0300

    Add files via upload

---
+++ Lab3 (C++).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double s, x, e, demtor, numtor, deltS;
+    int n, sign;
+    n = 0;
+    s = 0;
+    e = 0.00001;
+    deltS = 1;
+    cout << "Enter x: "; cin >> x;
+    cout.setf(ios_base::fixed);
+    while (abs(deltS)  > e) {
+        sign = pow((-1), n);
+        numtor = pow(x, 2 * n) + 1;
+        demtor = pow(2, n) + 1;
+        deltS = sign * numtor / demtor;
+        s = s + deltS;
+        n++;
+    }
+
+    cout << "Result: " << s;
+}
